aab6e3bd9e3d84d40e41a53bb05b8ea037bd70f1
franly034 <franklynugege@gmail.com>
2018-12-01 13:26:32 +0100

    Defined our config/paystack module with initializePayment and verifyPayment methods

---
+++ config/paystack.js
@@ -0,0 +1,37 @@
+const paystack = (request) => {
+    const MySecretKey = '';
+    const initializePayment = (form, mycallback) => {
+        const options = {
+            url : 'https://api.paystack.co/transaction/initialize',
+            headers : {
+                authorization: MySecretKey,
+                'content-type': 'application/json',
+                'cache-control': 'no-cache'    
+            },
+            form
+        }
+        const callback = (error, response, body) => {
+            return mycallback(error, body)
+        }
+        request.post(options, callback)
+    }
+
+    const verifyPayment = (ref, mycallback) => {
+        const options = {
+            url : 'https://api.paystack.co/transaction/verify/'+encodeURIComponent(ref),
+            headers : {
+                authorization: MySecretKey,
+                'content-type': 'application/json',
+                'cache-control': 'no-cache'    
+            }
+        }
+        const callback = (error, response, body) => {
+            return mycallback(error, body)
+        }
+        request(options, callback)
+    }
+
+    return {initializePayment, verifyPayment};
+}
+
+module.exports = paystack;
